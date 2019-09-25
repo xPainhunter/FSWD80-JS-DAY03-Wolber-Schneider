7b5000dc01ae85b89444ed79ce8cc0f1ae8d60a9
LadyDuck <birgit.schneider@chello.at>
2019-09-25 09:18:45 +0200

    add web shop

---
+++ Web Shop/store.js
@@ -0,0 +1,10 @@
+var removeCartItemButtons = document.getElementsByClassName("btn-danger");
+console.log(removeCartItemButtons);
+
+for(var i = 0; i < removeCartItemButtons.length; i++) {
+	var button = removeCartItemButtons[i];
+	button.addEventListener('click', function(event) {
+		var buttonClicked = event.target;
+		buttonClicked.parentElement
+	})
+}
